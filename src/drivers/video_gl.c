0bff6e44cbe664a249cabd7b4b8f8cf8ad5d1b46
nkeynes <nkeynes@localhost>
2007-01-14 02:55:06 +0000

    Add GL common file

---
+++ src/drivers/video_gl.c
@@ -0,0 +1,72 @@
+/**
+ * $Id: video_gl.c,v 1.1 2007-01-14 02:55:06 nkeynes Exp $
+ *
+ * Common GL code that doesn't depend on a specific implementation
+ *
+ * Copyright (c) 2005 Nathan Keynes.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "dream.h"
+#include "drivers/video_gl.h"
+#include <GL/gl.h>
+
+char *required_extensions[] = { "GL_EXT_framebuffer_object", NULL };
+
+/**
+ * Test if a specific extension is supported. From opengl.org
+ * @param extension extension name to check for
+ * @return TRUE if supported, otherwise FALSE.
+ */
+gboolean isGLExtensionSupported( const char *extension )
+{
+    const GLubyte *extensions = NULL;
+    const GLubyte *start;
+    GLubyte *where, *terminator;
+
+    /* Extension names should not have spaces. */
+    where = (GLubyte *) strchr(extension, ' ');
+    if (where || *extension == '\0')
+	return 0;
+    extensions = glGetString(GL_EXTENSIONS);
+    /* It takes a bit of care to be fool-proof about parsing the
+       OpenGL extensions string. Don't be fooled by sub-strings,
+       etc. */
+    start = extensions;
+    for (;;) {
+	where = (GLubyte *) strstr((const char *) start, extension);
+	if (!where)
+	    break;
+	terminator = where + strlen(extension);
+	if (where == start || *(where - 1) == ' ')
+	    if (*terminator == ' ' || *terminator == '\0')
+		return TRUE;
+	start = terminator;
+    }
+    return FALSE;
+}
+
+gboolean hasRequiredGLExtensions( ) 
+{
+    int i;
+    gboolean isOK = TRUE;
+    fprintf( stdout, "GL Extensions: %s\n", glGetString(GL_EXTENSIONS) );
+
+
+    for( i=0; required_extensions[i] != NULL; i++ ) {
+	if( !isGLExtensionSupported(required_extensions[i]) ) {
+	    ERROR( "Required OpenGL extension not supported: %s", required_extensions[i] );
+	    isOK = FALSE;
+	}
+    }
+    return isOK;
+}
