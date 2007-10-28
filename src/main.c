307a7bdb91d792d1ba9ef75cadb23f68c176016f
nkeynes <nkeynes@localhost>
2007-10-28 07:36:11 +0000

    Make the sure initial GD (if any) makes its way to the gui

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /**
- * $Id: main.c,v 1.31 2007-10-27 05:44:54 nkeynes Exp $
+ * $Id: main.c,v 1.32 2007-10-28 07:36:11 nkeynes Exp $
  *
  * Main program, initializes dreamcast and gui, then passes control off to
  * the gtk main loop (currently). 
@@ -178,7 +178,7 @@ int main (int argc, char *argv[])
     INFO( "%s! ready...", APP_NAME );
 
     for( ; optind < argc; optind++ ) {
-	gboolean ok = gdrom_mount_image(argv[optind]);
+	gboolean ok = gdrom_menu_open_file(argv[optind]);
 	if( !ok ) {
 	    ok = file_load_magic( argv[optind] );
 	}
@@ -195,7 +195,7 @@ int main (int argc, char *argv[])
     if( gdrom_get_current_disc() == NULL ) {
 	disc_file = lxdream_get_config_value( CONFIG_GDROM );
 	if( disc_file != NULL ) {
-	    gdrom_mount_image( disc_file );
+	    gdrom_menu_open_file( disc_file );
 	}
     } else {
 	lxdream_set_global_config_value( CONFIG_GDROM, gdrom_get_current_disc()->name );
