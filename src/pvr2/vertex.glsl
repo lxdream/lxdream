fe12747d46e97ca1df5323a308fe00aefce840bd
nkeynes <nkeynes@localhost>
2008-03-26 11:56:12 +0000

    Fix perspective correction (at least for those with shader support)

--- src/pvr2/vertex.glsl
+++ src/pvr2/vertex.glsl
@@ -2,8 +2,9 @@
 
 void main()
 {
-    gl_Position = ftransform();
-//    gl_Position.z = log(gl_Vertex.z);
+    vec4 tmp = ftransform();
+    float w = gl_Vertex.z;
+    gl_Position  = tmp * w;
     gl_FrontColor = gl_Color;
     gl_FrontSecondaryColor = gl_SecondaryColor;
     gl_TexCoord[0] = gl_MultiTexCoord0;
