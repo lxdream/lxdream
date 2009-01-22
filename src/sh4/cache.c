a0f61342d3e93385db3022860466daf546525e18
nkeynes <nkeynes@localhost>
2009-01-22 02:58:13 +0000

    Fix 1k-entry allocation Break asid remap into two passes for simplicity

--- src/sh4/cache.c
+++ src/sh4/cache.c
@@ -21,6 +21,7 @@
 #include "dream.h"
 #include "mem.h"
 #include "mmio.h"
+#include "clock.h"
 #include "sh4/sh4core.h"
 #include "sh4/sh4mmio.h"
 #include "sh4/xltcache.h"
@@ -43,12 +44,21 @@ struct cache_line {
 
 
 static struct cache_line ccn_icache[ICACHE_ENTRY_COUNT];
-static struct cache_line ccn_ocache[OCACHE_ENTRY_COUNT];
+struct cache_line ccn_ocache[OCACHE_ENTRY_COUNT];
 static unsigned char ccn_icache_data[ICACHE_ENTRY_COUNT*32];
-static unsigned char ccn_ocache_data[OCACHE_ENTRY_COUNT*32];
+unsigned char ccn_ocache_data[OCACHE_ENTRY_COUNT*32];
 
 
 /*********************** General module requirements ********************/
+
+void CCN_reset()
+{
+    /* Clear everything for consistency */
+    memset( ccn_icache, 0, sizeof(ccn_icache) );
+    memset( ccn_ocache, 0, sizeof(ccn_icache) );
+    memset( ccn_icache_data, 0, sizeof(ccn_icache) );
+    memset( ccn_ocache_data, 0, sizeof(ccn_icache) );
+}
 
 void CCN_save_state( FILE *f )
 {
@@ -165,6 +175,141 @@ struct mem_region_fn mem_region_ocram_page1 = {
         ocram_page1_read_burst, ocram_page1_write_burst,
         unmapped_prefetch };
 
+/**************************** Cache functions ********************************/
+char ccn_cache_map[16 MB]; // 24 bits of address space
+
+/**
+ * Load a 32-byte cache line from external memory at the given ext address.
+ * @param addr external address pre-masked to 1FFFFFFE0 
+ */
+sh4addr_t FASTCALL ccn_ocache_load_line( sh4addr_t addr )
+{
+    int entry = addr & 0x00003FE0;
+    struct cache_line *line = &ccn_ocache[entry>>5];
+    char *cache_data = &ccn_ocache_data[entry];
+    sh4addr_t old_addr = line->tag;
+    line->tag = addr & 0x1FFFFFE0;
+    char oldstate = ccn_cache_map[old_addr>>5];
+    ccn_cache_map[old_addr>>5] = 0;
+    ccn_cache_map[addr>>5] = CACHE_VALID;
+    if( oldstate == (CACHE_VALID|CACHE_DIRTY) ) {
+        // Cache line is dirty - writeback. 
+        ext_address_space[old_addr>>12]->write_burst(old_addr, cache_data);
+    }
+    ext_address_space[addr>>12]->read_burst(cache_data, addr & 0x1FFFFFE0);
+    return addr;
+}
+
+/* Long read through the operand cache */
+/*
+int32_t FASTCALL ccn_ocache_read_long( sh4addr_t addr );
+int32_t FASTCALL ccn_ocache_read_word( sh4addr_t addr );
+int32_t FASTCALL ccn_ocache_read_byte( sh4addr_t addr );
+void FASTCALL ccn_ocache_write_long_copyback( sh4addr_t addr, uint32_t val );
+void FASTCALL ccn_ocache_write_word_copyback( sh4addr_t addr, uint32_t val );
+void FASTCALL ccn_ocache_write_byte_copyback( sh4addr_t addr, uint32_t val );
+
+*/
+static int32_t FASTCALL ccn_ocache_read_long( sh4addr_t addr )
+{
+    addr &= 0x1FFFFFFF;
+    if( (ccn_cache_map[addr>>5] & CACHE_VALID) == 0 ) {
+        ccn_ocache_load_line(addr);
+    }
+    return *(int32_t *)&ccn_ocache_data[addr & 0x3FFF];
+}
+
+static int32_t FASTCALL ccn_ocache_read_word( sh4addr_t addr )
+{
+    addr &= 0x1FFFFFFF;
+    if( (ccn_cache_map[addr>>5] & CACHE_VALID) == 0 ) {
+        ccn_ocache_load_line(addr);
+    }
+    return SIGNEXT16(*(int16_t *)&ccn_ocache_data[addr&0x3FFF]);    
+}
+
+static int32_t FASTCALL ccn_ocache_read_byte( sh4addr_t addr )
+{
+    addr &= 0x1FFFFFFF;
+    if( (ccn_cache_map[addr>>5] & CACHE_VALID) == 0 ) {
+        ccn_ocache_load_line(addr);
+    }
+    return SIGNEXT8(ccn_ocache_data[addr&0x3FFF]);        
+}
+
+static void FASTCALL ccn_ocache_write_long_copyback( sh4addr_t addr, uint32_t value )
+{
+    addr &= 0x1FFFFFFF;
+    if( (ccn_cache_map[addr>>5] & CACHE_VALID) == 0 ) {
+        ccn_ocache_load_line(addr);
+    }
+    ccn_cache_map[addr>>5] |= CACHE_DIRTY;
+    *(uint32_t *)&ccn_ocache_data[addr&0x3FFF] = value;
+}
+
+static void FASTCALL ccn_ocache_write_word_copyback( sh4addr_t addr, uint32_t value )
+{
+    addr &= 0x1FFFFFFF;
+    if( (ccn_cache_map[addr>>5] & CACHE_VALID) == 0 ) {
+        ccn_ocache_load_line(addr);
+    }
+    ccn_cache_map[addr>>5] |= CACHE_DIRTY;
+    *(uint16_t *)&ccn_ocache_data[addr&0x3FFF] = (uint16_t)value;
+}
+
+static void FASTCALL ccn_ocache_write_byte_copyback( sh4addr_t addr, uint32_t value )
+{
+    addr &= 0x1FFFFFFF;
+    if( (ccn_cache_map[addr>>5] & CACHE_VALID) == 0 ) {
+        ccn_ocache_load_line(addr);
+    }
+    ccn_cache_map[addr>>5] |= CACHE_DIRTY;
+    ccn_ocache_data[addr&0x3FFF] = (uint8_t)value;
+}
+
+static void FASTCALL ccn_ocache_prefetch( sh4addr_t addr )
+{
+    addr &= 0x1FFFFFFF;
+    if( (ccn_cache_map[addr>>5] & CACHE_VALID) == 0 ) {
+        ccn_ocache_load_line(addr);
+    }
+}
+
+void FASTCALL ccn_ocache_invalidate( sh4addr_t addr )
+{
+    addr &= 0x1FFFFFFF;
+    ccn_cache_map[addr>>5] &= ~CACHE_VALID;
+}
+
+void FASTCALL ccn_ocache_purge( sh4addr_t addr )
+{
+    addr &= 0x1FFFFFE0;
+    int oldflags = ccn_cache_map[addr>>5]; 
+    ccn_cache_map[addr>>5] &= ~CACHE_VALID;
+    if( oldflags == (CACHE_VALID|CACHE_DIRTY) ) {
+        char *cache_data = &ccn_ocache_data[addr & 0x3FE0];
+        ext_address_space[addr>>12]->write_burst(addr, cache_data);
+    }
+}
+
+void FASTCALL ccn_ocache_writeback( sh4addr_t addr )
+{
+    addr &= 0x1FFFFFE0;
+    if( ccn_cache_map[addr>>5] == (CACHE_VALID|CACHE_DIRTY) ) {
+        ccn_cache_map[addr>>5] &= ~CACHE_DIRTY;
+        char *cache_data = &ccn_ocache_data[addr & 0x3FE0];
+        ext_address_space[addr>>12]->write_burst(addr, cache_data);
+    }
+}
+
+struct mem_region_fn ccn_ocache_cb_region = {
+    ccn_ocache_read_long, ccn_ocache_write_long_copyback,
+    ccn_ocache_read_word, ccn_ocache_write_word_copyback,
+    ccn_ocache_read_byte, ccn_ocache_write_byte_copyback,
+    unmapped_read_burst, unmapped_write_burst,
+    ccn_ocache_prefetch };
+
+
 /************************** Cache direct access ******************************/
 
 static int32_t FASTCALL ccn_icache_addr_read( sh4addr_t addr )
@@ -212,11 +357,11 @@ struct mem_region_fn p4_region_icache_data = {
         unmapped_read_burst, unmapped_write_burst,
         unmapped_prefetch };
 
-
 static int32_t FASTCALL ccn_ocache_addr_read( sh4addr_t addr )
 {
     int entry = (addr & 0x00003FE0);
-    return ccn_ocache[entry>>5].tag;
+    sh4addr_t tag = ccn_ocache[entry>>5].tag;
+    return (tag&0x1FFFFC00) | ccn_cache_map[tag>>5];
 }
 
 static void FASTCALL ccn_ocache_addr_write( sh4addr_t addr, uint32_t val )
@@ -225,13 +370,14 @@ static void FASTCALL ccn_ocache_addr_write( sh4addr_t addr, uint32_t val )
     struct cache_line *line = &ccn_ocache[entry>>5];
     if( addr & 0x08 ) { // Associative
     } else {
-        if( (line->tag & (CACHE_VALID|CACHE_DIRTY)) == (CACHE_VALID|CACHE_DIRTY) ) {
-            unsigned char *cache_data = &ccn_ocache_data[entry&0x00003FE0];
+        sh4addr_t tag = line->tag;
+        if( ccn_cache_map[tag>>5] == (CACHE_VALID|CACHE_DIRTY) ) {
             // Cache line is dirty - writeback. 
-            ext_address_space[line->tag>>12]->write_burst(line->key, cache_data);
+            unsigned char *cache_data = &ccn_ocache_data[entry];
+            ext_address_space[tag>>12]->write_burst(tag, cache_data);
         }
-        line->tag = val & 0x1FFFFC03;
-        line->key = (val & 0x1FFFFC00)|(entry & 0x000003E0);
+        line->tag = tag = (val & 0x1FFFFC00) | (addr & 0x3E0);
+        ccn_cache_map[tag>>5] = val & 0x03;
     }
 }
 
@@ -271,12 +417,14 @@ void CCN_set_cache_control( int reg )
     
     if( reg & CCR_ICI ) { /* icache invalidate */
         for( i=0; i<ICACHE_ENTRY_COUNT; i++ ) {
+            ccn_icache[i].key = -1;
             ccn_icache[i].tag &= ~CACHE_VALID;
         }
     }
     
     if( reg & CCR_OCI ) { /* ocache invalidate */
         for( i=0; i<OCACHE_ENTRY_COUNT; i++ ) {
+            ccn_icache[i].key = -1;
             ccn_ocache[i].tag &= ~(CACHE_VALID|CACHE_DIRTY);
         }
     }
@@ -311,13 +459,55 @@ void FASTCALL ccn_prefetch( sh4addr_t addr )
     
 }
 
-/**
- * Prefetch for non-cached regions. Oddly enough, this does nothing whatsoever.
- */
+/************************** Uncached memory access ***************************/
+int32_t FASTCALL ccn_uncached_read_long( sh4addr_t addr )
+{
+    sh4r.slice_cycle += (4*sh4_bus_period);
+    addr &= 0x1FFFFFFF;
+    return ext_address_space[addr>>12]->read_long(addr);
+}
+int32_t FASTCALL ccn_uncached_read_word( sh4addr_t addr )
+{
+    sh4r.slice_cycle += (4*sh4_bus_period);
+    addr &= 0x1FFFFFFF;
+    return ext_address_space[addr>>12]->read_word(addr);
+}
+int32_t FASTCALL ccn_uncached_read_byte( sh4addr_t addr )
+{
+    sh4r.slice_cycle += (4*sh4_bus_period);
+    addr &= 0x1FFFFFFF;
+    return ext_address_space[addr>>12]->read_byte(addr);
+}
+void FASTCALL ccn_uncached_write_long( sh4addr_t addr, uint32_t val )
+{
+    sh4r.slice_cycle += (4*sh4_bus_period);
+    addr &= 0x1FFFFFFF;
+    return ext_address_space[addr>>12]->write_long(addr, val);
+}
+void FASTCALL ccn_uncached_write_word( sh4addr_t addr, uint32_t val )
+{
+    sh4r.slice_cycle += (4*sh4_bus_period);
+    addr &= 0x1FFFFFFF;
+    return ext_address_space[addr>>12]->write_word(addr, val);
+}
+void FASTCALL ccn_uncached_write_byte( sh4addr_t addr, uint32_t val )
+{
+    sh4r.slice_cycle += (4*sh4_bus_period);
+    addr &= 0x1FFFFFFF;
+    return ext_address_space[addr>>12]->write_byte(addr, val);
+}
 void FASTCALL ccn_uncached_prefetch( sh4addr_t addr )
 {
-    
 }
+
+struct mem_region_fn ccn_uncached_region = {
+        ccn_uncached_read_long, ccn_uncached_write_long,
+        ccn_uncached_read_word, ccn_uncached_write_word,
+        ccn_uncached_read_byte, ccn_uncached_write_byte,
+        unmapped_read_burst, unmapped_write_burst,
+        ccn_uncached_prefetch };
+
+
 /********************************* Store-queue *******************************/
 /*
  * The storequeue is strictly speaking part of the cache, but most of 
@@ -358,3 +548,4 @@ void FASTCALL ccn_storequeue_prefetch_tlb( sh4addr_t addr )
     sh4ptr_t src = (sh4ptr_t)&sh4r.store_queue[queue];
     ext_address_space[addr>>12]->write_burst( (addr & 0x1FFFFFE0), src );
 }
+
