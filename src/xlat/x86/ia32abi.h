c8a1cf5bd75562a18b5cb133d567ae6c529d76aa
Nathan Keynes <nkeynes@lxdream.org>
2009-04-04 10:17:57 +0000

    Remove last lingering bits of SH4-specific code from the ABI headers

--- src/xlat/x86/ia32abi.h
+++ src/xlat/x86/ia32abi.h
@@ -26,9 +26,8 @@
 #define REG_RESULT1 REG_EAX
 #define MAX_REG_ARG 2
 
-static inline void decode_address( int addr_reg )
+static inline void decode_address( uintptr_t base, int addr_reg )
 {
-    uintptr_t base = (sh4r.xlat_sh4_mode&SR_MD) ? (uintptr_t)sh4_address_space : (uintptr_t)sh4_user_address_space;
     MOVL_r32_r32( addr_reg, REG_ECX );
     SHRL_imm_r32( 12, REG_ECX ); 
     MOVP_sib_rptr( 2, REG_ECX, -1, base, REG_ECX );
@@ -142,7 +141,6 @@ static inline void CALL3_r32disp_r32_r32_r32( int preg, uint32_t disp, int arg1,
 static inline void enter_block( ) 
 {
     PUSH_r32(REG_EBP);
-    MOVP_immptr_rptr( ((uint8_t *)&sh4r) + 128, REG_EBP );
     SUBL_imms_r32( 8, REG_ESP ); 
 }
 
