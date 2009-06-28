64fda96944e779aef572268bb3fd6efe1edfc929
Nathan Keynes <nkeynes@lxdream.org>
2009-06-28 01:45:16 +0000

    Change LIRC data structure to use GData (patch by Wahrhaft)

--- src/drivers/input_lirc.c
+++ src/drivers/input_lirc.c
@@ -45,11 +45,15 @@ typedef struct input_lirc {
     GIOChannel *channel;
 } *input_lirc_t;
 
-#define MAX_KEYSYMS 256
-static char *keysyms[MAX_KEYSYMS];
+#define MAX_KEYSYMS 65536
+static GData *keysyms;
+static uint16_t last_keycode;
+static GQuark keysym_by_keycode_result;
+
 
 static uint16_t input_lirc_resolve_keysym( input_driver_t dev, const gchar *str );
 static gchar *input_lirc_keysym_for_keycode( input_driver_t dev, uint16_t keycode );
+static void get_keysym_by_keycode(GQuark key_id, gpointer data, gpointer user_data);
 static gboolean input_lirc_callback( GIOChannel *source, GIOCondition condition, gpointer data );
 
 input_driver_t system_lirc_driver;
@@ -68,11 +72,11 @@ static gboolean input_lirc_init()
         WARN("Could not initialize LIRC.  LIRC hotkeys will be disabled.");
         return FALSE;
     }
-    
+
     system_lirc_driver->channel = g_io_channel_unix_new(system_lirc_driver->fd);
     g_io_channel_set_flags(system_lirc_driver->channel, G_IO_FLAG_IS_READABLE | G_IO_FLAG_NONBLOCK, NULL);
     g_io_add_watch(system_lirc_driver->channel, G_IO_IN|G_IO_ERR|G_IO_HUP, input_lirc_callback, system_lirc_driver);
-    memset(keysyms, 0, MAX_KEYSYMS);
+    g_datalist_init(&keysyms);
     input_register_device((input_driver_t)system_lirc_driver, MAX_KEYSYMS - 1);
     return TRUE;
 }
@@ -90,23 +94,18 @@ static uint16_t input_lirc_resolve_keysym( input_driver_t dev, const gchar *str 
 {
     //LIRC uses keysyms but no keycodes.  To generate a keycode, we'll just make them up as we go.
     //As long as we store keysyms instead of keycodes in any config files, this should be fine.
-    uint16_t i;
-    for (i = 1; i < MAX_KEYSYMS && keysyms[i] != NULL; i++) {
-        if (strcasecmp(str, keysyms[i]) == 0) {
-            //keycode already exists
-            return i;
-        }
-    }
-    
-    if (i < MAX_KEYSYMS) {
+
+    uint16_t keycode;
+    keycode = (uint16_t)GPOINTER_TO_INT(g_datalist_get_data(&keysyms, str));
+
+    if (keycode == 0) {
         //this key is not in the list yet, so make a new keycode for it
-        keysyms[i] = g_strdup(str);
-        return i;
+        g_datalist_set_data(&keysyms, str, GINT_TO_POINTER(++last_keycode));
+        return last_keycode;
     } else {
-        //if your remote has more than 256 buttons, you may need to increase MAX_KEYSYMS
-        ERROR("LIRC has too many keysyms!");
-        return 0;
+        return keycode;
     }
+
 }
 
 static gchar *input_lirc_keysym_for_keycode( input_driver_t dev, uint16_t keycode )
@@ -114,21 +113,30 @@ static gchar *input_lirc_keysym_for_keycode( input_driver_t dev, uint16_t keycod
     //This won't work if you send in keycodes that haven't been fired by the callback
     //or looked up using resolve_keysym yet.  This shouldn't be a problem, since these
     //two functions are the only way to get a keycode in the first place.
-    
-    if (keycode < MAX_KEYSYMS)
-        return g_strdup(keysyms[keycode]);
-    else
+
+    if (keycode <= last_keycode) {
+        //reverse lookup
+        keysym_by_keycode_result = 0;
+        g_datalist_foreach(&keysyms, get_keysym_by_keycode, GINT_TO_POINTER(keycode));
+        return g_strdup(g_quark_to_string(keysym_by_keycode_result));
+    } else {
         return NULL;
+    }
+}
+
+static void get_keysym_by_keycode(GQuark key_id, gpointer data, gpointer user_data)
+{
+    if (data == user_data)
+        keysym_by_keycode_result = key_id;
 }
 
 static gboolean input_lirc_callback( GIOChannel *source, GIOCondition condition, gpointer data )
 {
     int ret;
     char *code, *c;
-    char keysym[256];
-    
+
     input_lirc_t lirc = (input_lirc_t)data;
-    
+
     if (condition & G_IO_IN)
     {
         //loop through all queued commands
@@ -137,13 +145,13 @@ static gboolean input_lirc_callback( GIOChannel *source, GIOCondition condition,
             INFO("LIRC code (%s)", code);
             //code contains id, repeat count, and keysym separated by spaces
             gchar **code_split = g_strsplit(code, " ", 4);
-            
+
             //eliminate repeats by only accepting the first instance of a keysym
             if (atoi(code_split[1]) == 0)
             {
                 input_event_keydown((input_driver_t)lirc, input_lirc_resolve_keysym((input_driver_t)lirc, code_split[2]), MAX_PRESSURE);
             }
-            
+
             g_strfreev(code_split);
             free(code);
         }
