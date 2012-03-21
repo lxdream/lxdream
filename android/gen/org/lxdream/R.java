2eb0e5232be5d73f9919a56aad5d67fadfe5a4c8
nkeynes <nkeynes@localhost>
2012-03-21 20:36:20 +1000

    Add app icon

--- android/gen/org/lxdream/R.java
+++ android/gen/org/lxdream/R.java
@@ -11,11 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int tb_cdrom=0x7f020000;
-        public static final int tb_pause=0x7f020001;
-        public static final int tb_preferences=0x7f020002;
-        public static final int tb_reset=0x7f020003;
-        public static final int tb_run=0x7f020004;
+        public static final int lxdream=0x7f020000;
+        public static final int tb_cdrom=0x7f020001;
+        public static final int tb_pause=0x7f020002;
+        public static final int tb_preferences=0x7f020003;
+        public static final int tb_reset=0x7f020004;
+        public static final int tb_run=0x7f020005;
     }
     public static final class id {
         public static final int menu_reset=0x7f050001;
